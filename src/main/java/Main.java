fcb68bc329bb62e351689277abab34e674014ad5
samsthenjwa <you@example.com>
2020-04-27 20:22:48 +0200

    commited

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+class GenericClass<T>{
+        private T _value;
+
+        public void set(T value){
+            _value = value;
+        }
+        public T get(){
+            return _value;
+        }
+
+        public <T> void printArrayContent(T[] array){
+            for (T elements:array) {
+                System.out.print(elements + " "); // Print both integer and/or string elements
+            }
+            System.out.println("");
+        }
+    }
+
+    public class Main {
+        public static void main(String[] args) {
+
+            GenericClass obj = new GenericClass();
+
+            obj.printArrayContent(new Integer[]{1,2,3});
+            obj.printArrayContent(new String[]{"Hello ", "This is ", "Me"});
+
+            GenericClass<String> stringInstance = new GenericClass<String>();
+            stringInstance.set("1212"); //Assign a string value to Generic class value
+
+            System.out.println(stringInstance.get()); // print out Test
+
+            GenericClass<Integer> integerInstance = new GenericClass<Integer>();
+            integerInstance.set(1000); //Assign a integer value to Generic class value
+
+            System.out.println(integerInstance.get()); // print out 1000
+
+        }
+}
